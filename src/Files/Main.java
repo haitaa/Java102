7285080d311d73486d166cc914e139e6f257f517
Mustafa Haita <mustafahaitaa@gmail.com>
2023-12-09 00:46:25 +0300

    File Class

---
+++ src/Files/Main.java
@@ -0,0 +1,26 @@
+package Files;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        File dosya = new File("src/Files/patika.txt");
+        try {
+            if(dosya.createNewFile()) {
+                System.out.println(dosya.getName() + " dosyası oluşturuldu.");
+            } else {
+                System.out.println(dosya.getName() + " dosyası zaten mevcut.");
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        File dizin = new File("test");
+        dizin.mkdir();
+
+        */
+
+    }
+}
